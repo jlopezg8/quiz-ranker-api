02cde96f2421be872fe878d23818481f04139413
jlopezg8 <jlopezg8@gmail.com>
2022-06-17 11:31:09 -0500

    Modified the operations IDs

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from fastapi.routing import APIRoute
 
 from routers import auth_router, test_router
 
-app = FastAPI()
+
+# https://fastapi.tiangolo.com/advanced/generate-clients/?h=sdk#client-method-names
+def custom_generate_unique_id(route: APIRoute):
+    return route.name
+
+
+app = FastAPI(generate_unique_id_function=custom_generate_unique_id)
 app.include_router(auth_router)
 app.include_router(test_router)
